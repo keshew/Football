3f4defa7586a8846b06ba4fdac58958d2283b5c0
keshew <artyom.korotckov2017@yandex.ru>
2025-06-05 18:43:10 +0500

    Initial Commit

---
+++ Football/FootballApp.swift
@@ -0,0 +1,17 @@
+//
+//  FootballApp.swift
+//  Football
+//
+//  Created by Артём Коротков on 05.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct FootballApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
